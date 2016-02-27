46b1ebe171ab755f1b54ad752baa97da2a1a6509
Alex Sobolev <alexaustrali125@gmail.com>
2016-02-28 01:54:18 +0500

    Base template Metronic installed

---
+++ resources/views/app.blade.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+<!-- BEGIN HEAD -->
+
+<head>
+    <meta charset="utf-8" />
+    <title>@section('title') Hitman @show</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+
+    @section('meta_description')<meta content="" name="description" />@show
+    @section('meta_author')<meta content="" name="author" />@show
+
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('theme/global/plugins/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('theme/global/plugins/simple-line-icons/simple-line-icons.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('theme/global/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('theme/global/plugins/uniform/css/uniform.default.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('theme/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- END GLOBAL MANDATORY STYLES -->
+    <!-- BEGIN THEME GLOBAL STYLES -->
+    <link href="{{ asset('theme/global/css/components.min.css') }}" rel="stylesheet" id="style_components" type="text/css" />
+    <link href="{{ asset('theme/global/css/plugins.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- END THEME GLOBAL STYLES -->
+    <!-- BEGIN THEME LAYOUT STYLES -->
+    <link href="{{ asset('theme/layouts/layout2/css/layout.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('theme/layouts/layout2/css/themes/blue.min.css') }}" rel="stylesheet" type="text/css" id="style_color" />
+    <link href="{{ asset('theme/layouts/layout2/css/custom.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- END THEME LAYOUT STYLES -->
+
+    <!-- BEGIN CUSTOM STYLES -->
+    @yield('styles')
+    <!-- END CUSTOM STYLES -->
+
+    <link rel="shortcut icon" href="favicon.ico" />
+</head>
+<!-- END HEAD -->
+
+<body class="page-header-fixed page-sidebar-closed-hide-logo page-container-bg-solid">
+
+    <!-- BEGIN HEADER -->
+    @include('layout.header')
+    <!-- END HEADER -->
+
+    <!-- BEGIN HEADER & CONTENT DIVIDER -->
+    <div class="clearfix"> </div>
+    <!-- END HEADER & CONTENT DIVIDER -->
+    <!-- BEGIN CONTAINER -->
+    <div class="page-container">
+
+        <!-- BEGIN SIDEBAR -->
+        @include('layout.sidebar-menu')
+        <!-- END SIDEBAR -->
+
+        <!-- BEGIN CONTENT -->
+        @include('layout.content')
+        <!-- END CONTENT -->
+
+        <!-- BEGIN QUICK SIDEBAR -->
+        @include('layout.quick-sidebar')
+        <!-- END QUICK SIDEBAR -->
+
+    </div>
+    <!-- END CONTAINER -->
+    <!-- BEGIN FOOTER -->
+    <div class="page-footer">
+        <div class="page-footer-inner"> 2016 &copy; Hitman</div>
+        <div class="scroll-to-top">
+            <i class="icon-arrow-up"></i>
+        </div>
+    </div>
+    <!-- END FOOTER -->
+    <!--[if lt IE 9]>
+    <script src="{{ asset('theme/global/plugins/respond.min.js') }}"></script>
+    <script src="{{ asset('theme/global/plugins/excanvas.min.js') }}"></script>
+    <![endif]-->
+    <!-- BEGIN CORE PLUGINS -->
+    <script src="{{ asset('theme/global/plugins/jquery.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('theme/global/plugins/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('theme/global/plugins/js.cookie.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('theme/global/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('theme/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('theme/global/plugins/jquery.blockui.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('theme/global/plugins/uniform/jquery.uniform.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('theme/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js') }}" type="text/javascript"></script>
+    <!-- END CORE PLUGINS -->
+    <!-- BEGIN THEME GLOBAL SCRIPTS -->
+    <script src="{{ asset('theme/global/scripts/app.min.js') }}" type="text/javascript"></script>
+    <!-- END THEME GLOBAL SCRIPTS -->
+    <!-- BEGIN THEME LAYOUT SCRIPTS -->
+    <script src="{{ asset('theme/layouts/layout2/scripts/layout.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('theme/layouts/layout2/scripts/demo.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('theme/layouts/global/scripts/quick-sidebar.min.js') }}" type="text/javascript"></script>
+    <!-- END THEME LAYOUT SCRIPTS -->
+
+    <!-- BEGIN CUSTOM SCRIPTS -->
+    @yield('scripts')
+    <!-- END CUSTOM SCRIPTS -->
+
+</body>
+
+</html>
